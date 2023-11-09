a5bfed2e0115b20d0ef6ea34da0cc43f597fadd2
Angga Sya'ban Rahid <50766782+anggasybnrhd@users.noreply.github.com>
2023-11-09 20:59:22 +0700

    Add files via upload

---
+++ percabangan if else P3.php
@@ -0,0 +1,11 @@
+<?php
+
+$nilai = 80;
+if ($nilai >= 70) {
+    echo "Anda lulus!";
+} else {
+    echo "Anda gagal.";
+}
+
+//hasilnya lulus
+?>
---
+++ percabangan switch P3 .php
@@ -0,0 +1,16 @@
+<?php
+
+$hari = "Senin";
+switch ($hari) {
+    case "Senin":
+        echo "Hari kerja.";
+        break;
+    case "Sabtu":
+    case "Minggu":
+        echo "Hari libur.";
+        break;
+    default:
+        echo "Hari tidak valid.";
+}
+ // outputnya hari kerja
+?>
